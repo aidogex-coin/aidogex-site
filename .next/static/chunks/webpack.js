966f9e7827ce38e501162f8c8a4f68456fc6b8b1
salem aljabri <sms@salems-MacBook-Pro.local>
2025-03-23 11:28:53 -0400

    Fix imports and update hero section with Telegram button

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "12e2d2f1097e3482"; }
+/******/ 		__webpack_require__.h = function() { return "e556f4a809dbd0a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.aa6025864c8bbeac.hot-update.js
+++ .next/static/webpack/webpack.aa6025864c8bbeac.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1d5c442ae7e1aebd"; }
+/******/ 	__webpack_require__.h = function() { return "7c56617408c47159"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
